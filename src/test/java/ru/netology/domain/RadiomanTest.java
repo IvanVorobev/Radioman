488257838f264b49f56b646cbc398ee6dc488eb5
IvanVorobev <vivancho@list.ru>
2020-06-02 01:31:51 +0300

    Tests added

--- src/test/java/ru/netology/domain/RadiomanTest.java
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -133,4 +133,26 @@ class RadiomanTest {
 
         assertEquals(expected, radioman.getCurrentStation());
     }
+
+    @Test
+    void setCurrentVolumeOver() {
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(11);
+        int expected = 10;
+
+        assertEquals(expected, radioman.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeUnder() {
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(-1);
+        int expected = 0;
+
+        assertEquals(expected, radioman.getCurrentVolume());
+    }
 }
